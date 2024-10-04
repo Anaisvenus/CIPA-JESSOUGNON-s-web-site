2bc7c1a730bc6c8d4950940ce23bed0b348a845b
Anaisvenus <anais.asse-akakpo@epitech.eu>
2024-10-04 10:25:35 +0100

    fix: nex react project added because of displaying errors

---
+++ cipas-site/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './Views/LandingPage';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ cipas-site/src/Views/LandingPage.jsx
@@ -0,0 +1,17 @@
+import logo from "../Assets/logo.png"
+
+function LandingPage() {
+    return (
+      <div className="LandingPage h-screen">
+        <img
+        className=""
+        src={logo}
+        alt="Cipa's logo"/>
+
+        Hello
+        
+      </div>
+    );
+  }
+  
+  export default LandingPage;
